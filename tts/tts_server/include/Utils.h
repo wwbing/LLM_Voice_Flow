2d5410c448d706ef3b6e240f8e2c7120f6f6d232
sor-bai <bilibili.com>
2025-04-17 15:18:31 +0000

    [tts] Init tts code

---
+++ tts/tts_server/include/Utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <pthread.h>
+#include <vector>
+
+namespace utils
+{
+    bool set_realtime_priority(pthread_t thread_id, int priority_level);
+    bool is_valid_utf8_continuation(uint8_t c);
+    std::vector<std::string> split_long_text(const std::string &text, size_t max_length = 10);
+}
+
+#endif // UTILS_H
